c911f19ee31e5c48f1e755f111de049a9e827238
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-08-09 23:06:33 +0200

    Week 2, Lesson 5, Assignment 2, Task 1: Peer Graded Assignment: MongoDB and Mongoose
    Assignment Overview
    
    In this assignment, you will continue your journey with MongoDB and Mongoose. You will update the Dishes schema and model to support a few new fields in the schema. You will then create two new schemas for promotions and leadership, and test them by implementing a simple server. At the end of this assignment you would have completed the following three tasks:
    
    - Updated the Dishes schema and model to support some new fields and tested with a modified server.
    - Implemented the Promotions schema and model, and tested with a new server
    - Implemented the Leaders schema and model, and tested with a new server.
    
    Review criteria
    
    Upon completion of the assignment, your submission will be reviewed based on the following criteria:
    
    Task 1
    
    - The updated Dishes schema and model is correctly updated to support the new fields: image, category, label and price.
    - The label field is set to an empty string by default
    - The price schema is be supported with a new SchemaType called Currency.
    
    Task 2
    
    - The Promotions schema and model correctly supports all the fields as per the example document given above
    - The label field is set to an empty string by default
    - The price schema is be supported with a new SchemaType called Currency.
    
    Task 3
    
    - The Leaders schema and model correctly supports all the fields as per the example document given above.
    
    Changes to be committed:
     new file:   models/promotions.js
     modified:   server.js

--- node-mongoose/server.js
+++ node-mongoose/server.js
@@ -2,6 +2,16 @@ var mongoose = require('mongoose'),
     assert = require('assert');
 
 var Dishes = require('./models/dishes');
+var Promotions = require('./models/promotions');
+
+var dishesTestDone = false;
+var promotionsTestDone = false;
+
+var closeDb = function (db) {
+    if (dishesTestDone && promotionsTestDone) {
+        db.close();
+    }
+}
 
 // Connection URL
 var url = 'mongodb://localhost:27017/conFusion';
@@ -10,7 +20,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     // we're connected!
-    console.log("Connected correctly to server");
+    console.log("*** Connected correctly to server");
 
     // create a new dish
     Dishes.create({
@@ -29,7 +39,7 @@ db.once('open', function () {
         ]
     }, function (err, dish) {
         if (err) throw err;
-        console.log('Dish created!');
+        console.log('\n\n*** Dish created!');
         console.log(dish);
 
         // get all the dishes
@@ -37,7 +47,7 @@ db.once('open', function () {
             Dishes.findByIdAndUpdate(dish._id, {
                     $set: {
                         image: 'images/uthapizza.png',
-                        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',  
+                        description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
                         label: 'Hot'
                     }
                 }, {
@@ -45,7 +55,7 @@ db.once('open', function () {
                 })
                 .exec(function (err, dish) {
                     if (err) throw err;
-                    console.log('Updated Dish!');
+                    console.log('\n\n*** Updated Dish!');
                     console.log(dish);
 
                     dish.comments.push({
@@ -55,11 +65,49 @@ db.once('open', function () {
                     });
 
                     dish.save(function (err, dish) {
-                        console.log('Updated Comments!');
+                        console.log('\n\n*** Updated Comments!');
                         console.log(dish);
 
                         db.collection('dishes').drop(function () {
-                            db.close();
+                            dishesTestDone = true;
+                            closeDb(db);
+                        });
+                    });
+                });
+        }, 3000);
+    });
+
+    // create a new dish
+    Promotions.create({
+        name: 'Weekend Grand Buffet',
+        //image: will set the default value from the schema
+        category: 'mains',
+        //label: will set the default value from the schema
+        price: '19.99',
+        description: 'Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person'
+    }, function (err, dish) {
+        if (err) throw err;
+        console.log('\n\n*** Promotion created!');
+        console.log(dish);
+
+        // get all the promotions
+        setTimeout(function () {
+            Promotions.findByIdAndUpdate(dish._id, {
+                    $set: {
+                        image: 'images/buffet.png'
+                    }
+                }, {
+                    new: true
+                })
+                .exec(function (err, dish) {
+                    if (err) throw err;
+                    console.log('\n\n*** Updated Promotion!');
+                    console.log(dish);
+
+                    dish.save(function (err, dish) {
+                        db.collection('promotions').drop(function () {
+                            promotionsTestDone = true;
+                            closeDb(db);
                         });
                     });
                 });
