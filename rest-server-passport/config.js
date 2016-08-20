135ea16226bb226a8db29d66d693e5d08c2de861
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-08-20 22:31:35 +0200

    Week 4, Lesson 3, Exercise: Using OAuth with Passport and Facebook
    Objectives and Outcomes
    
    In this exercise you will make use of Passport OAuth support through the passport-facebook module together with Facebook's OAuth support to enable user authentication within your server. At the end of this exercise, you will be able to:
    
    - Configure your server to support user authentication based on OAuth providers
    - Use Passport OAuth support through the passport-facebook module to support OAuth based authentication with Facebook for your users.
    
    Conclusions
    
    In this exercise you learnt about using the Facebook OAuth support to enable authentication of your users and allowing them access to your server.
    
    Changes to be committed:
    	modified:   app.js
    	new file:   authenticate.js
    	modified:   config.js
    	modified:   models/user.js
    	modified:   package.json
    	modified:   routes/users.js

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '1779143745662615',
+        clientSecret: '75931c6900af09e60845c14f9afa225b',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
