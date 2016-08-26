ba9594a7239e8787f80205f10b56170fdbfaf834
VladimirDeminenko <vladimir.deminenko+github@gmail.com>
2016-08-26 22:23:05 +0200

    Changes to be committed: 	modified:   routes/dishRouter.js 	modified:   routes/favoriteRouter.js

--- rest-server-passport/routes/favoriteRouter.js
+++ rest-server-passport/routes/favoriteRouter.js
@@ -10,11 +10,14 @@ favoriteRouter.use(bodyParser.json());
 var Verify = require('./verify');
 
 favoriteRouter.route('/')
-    .all(Verify.verifyOrdinaryUser)
+    .all(Verify.verifyOrdinaryUser, function (req, res, next) {
+        req.body.owner = req.decoded._doc._id;
+        next();
+    })
     .get(function (req, res, next) {
         Favorites
             .find({
-                "owner": req.decoded._doc._id
+                "owner": req.body.owner
             })
             .populate('owner dishes')
             .exec(function (err, favorite) {
@@ -22,12 +25,10 @@ favoriteRouter.route('/')
                 res.json(favorite);
             });
     })
-    .post(function (req, res, next) {
+    .put(function (req, res, next) {
         // two steps:
         // - remove all user's favorites
         // - create a new user's favorite with dish collection
-        req.body.owner = req.decoded._doc._id;
-
         Favorites.remove({
             "owner": req.body.owner
         }, function (err, resp) {
@@ -46,7 +47,94 @@ favoriteRouter.route('/')
     })
     .delete(function (req, res, next) {
         Favorites.remove({
-            "owner": req.decoded._doc._id
+            "owner": req.body.owner
+        }, function (err, resp) {
+            if (err) throw err;
+            res.json(resp);
+        });
+    });
+
+favoriteRouter.route('/:id')
+    .all(Verify.verifyOrdinaryUser, function (req, res, next) {
+        req.body.owner = req.decoded._doc._id;
+        req.body.dish = req.params.id;
+
+        next();
+    })
+    .post(function (req, res, next) {
+        Favorites.find({
+            "owner": req.body.owner
+        }, function (err, favor) {
+            if (err) throw err;
+            var result = '';
+            var myObj = favor[0]['dishes'];
+
+            var idx = 0;
+            for (var field in myObj) {
+                idx++;
+                result += '\n' + idx + '(' + field + '): ' + myObj[field];
+            }
+
+            var dishesArr = favor[0].dishes;
+
+            result = '_id:\t' + favor[0]._id + '\nowner:\t' + favor[0].owner + '\ndishes:\t' + favor[0].dishes + '\ntypeof(dishes):\t'
+                + typeof (favor[0].dishes)
+                + 'dishesArr.length:\t' + dishesArr.length;
+
+
+
+            for (var i = 0; i < dishesArr.length; i++) {
+                var dish = dishesArr[i];
+
+                result += '\n(dishes[' + i + ']):\t' + dish;
+
+                if (dish === req.body.dish) {
+                    //                    result = dish;
+                    break;
+                } else {
+                    //result += '\n' + dish;
+                }
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+
+            res.end('***' + result + '\n***');
+        });
+        /** /
+                    favor.save(function (err, result) {
+                        if (err) throw err;
+                        res.writeHead(200, {
+                            'Content-Type': 'text/plain'
+                        });
+                        res.end('Deleted all comments!');
+                    });
+        /**/
+        /** /
+                // two steps:
+                // - remove all user's favorites
+                // - create a new user's favorite with dish collection
+                Favorites.remove({
+                    "owner": req.body.owner
+                }, function (err, resp) {
+                    if (err) throw err;
+
+                    Favorites.create(req.body, function (err, favorite) {
+                        if (err) throw err;
+
+                        res.writeHead(200, {
+                            'Content-Type': 'text/plain'
+                        });
+
+                        res.end('Created the favorite with id: ' + favorite._id);
+                    });
+                });
+        /**/
+    })
+    .delete(function (req, res, next) {
+        Favorites.remove({
+            "owner": req.body.owner
         }, function (err, resp) {
             if (err) throw err;
             res.json(resp);
